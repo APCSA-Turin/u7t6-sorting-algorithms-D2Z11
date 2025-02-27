e559c0aec912fc0117204f52daadf084f5bda35f
D2Z11 <133183204+D2Z11@users.noreply.github.com>
2025-02-27 13:40:02 +0000

    Working code

--- src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
+++ src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
@@ -1,45 +1,92 @@
 package com.example.project.Insertion_Sort;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class InsertionSort {
 
     // PART A. implementing insertion sort
     public static int[] insertionSort(int[] elements) {
-        for (int i = 0; i < elements.length; i++) {
-              for (int j = 0; j < i; j++) {
-                if (elements[j] > elements[i]) {
-                    int temp = elements[j];
-                    elements[j] = elements[i];
-                    elements[i] = temp;
-                }
-              }
+        int c = 0;
+        for (int i = 1; i < elements.length; i++) {
+            int j = i - 1;
+            int tempMoved = i;
+            while (j >= 0 && elements[tempMoved] < elements[j]) {
+                // System.out.println("SWAP: " + elements[j] + " and " + elements[tempMoved]);
+                // System.out.println(Arrays.toString(elements));
+                int temp = elements[j];
+                elements[j] = elements[tempMoved];
+                elements[tempMoved] = temp;
+                // System.out.println(Arrays.toString(elements));
+                c++;
+                j--;
+                tempMoved--;
+                // System.out.println(elements[tempMoved] + " " + elements[j]);
+            }
+            // if (j != i - 1) {
+            // System.out.println("SWAP: " + elements[j] + " and " + elements[i]);
+            // System.out.println(Arrays.toString(elements));
+            // int temp = elements[j];
+            // elements[j] = elements[i];
+            // elements[i] = temp;
+            // System.out.println(Arrays.toString(elements));
+            // }
         }
+        System.out.println(c);
         return elements;
     }
 
-   
     public static void selectionSort(int[] elements) {
-        
+        int c = 0;
+        // Skip last element since no items after it
+        for (int i = 0; i < elements.length - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < elements.length; j++) {
+                if (elements[j] < elements[maxIndex]) {
+                    maxIndex = j;
+                }
+                c++;
+            }
+            // swap i and j
+            int temp = elements[maxIndex];
+            elements[maxIndex] = elements[i];
+            elements[i] = temp;
+        }
+        System.out.println(c);
     }
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> insertionSortWordList(ArrayList<String> words) {
         for (int i = 0; i < words.size(); i++) {
             for (int j = 0; j < i; j++) {
-              if (words.get(j).compareTo(words.get(i)) > 0) {
-                  String temp = words.get(j);
-                  words.set(j, words.get(i));
-                  words.set(i, temp);
-              }
+                if (words.get(j).compareTo(words.get(i)) > 0) {
+                    String temp = words.get(j);
+                    words.set(j, words.get(i));
+                    words.set(i, temp);
+                }
             }
-      }
+        }
         return words;
     }
 
     public static void selectionSortWordList(ArrayList<String> words) {
+        for (int i = 0; i < words.size() - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < words.size(); j++) {
+                // Check if the current word is higher in terms of lexographic/alphabet order
+                if (words.get(j).compareTo(words.get(maxIndex)) < 0) {
+                    maxIndex = j;
+                }
+            }
+            // swap i and j
+            String temp = words.get(maxIndex);
+            words.set(maxIndex, words.get(i));
+            words.set(i, temp);
+            System.out.println(words);
+        }
     }
 
     public static ArrayList<String> loadWordsInto(ArrayList<String> wordList) {
@@ -55,5 +102,4 @@ public class InsertionSort {
         return wordList;
     }
 
-   
 }
---
+++ src/main/java/com/example/project/Insertion_Sort/Test.java
@@ -0,0 +1,28 @@
+package com.example.project.Insertion_Sort;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arrA = {40,46,63,36,98,81,8,95,77,30,47,18,70,15,49,61,94,32,39,48,96,100,74,44,88,55,86,58,9,31,22,87,11,75,68,97,7,21,2,79,14,59,62,16,26,34,60,83,85,38};
+// InsertionSort.selectionSort(arrA);
+// // System.out.println(Arrays.toString(arrA));
+// System.out.println();
+InsertionSort.selectionSort(arrA);
+System.out.println(Arrays.toString(arrA));
+// int[] arrB = {37,88,65,98,67,35,77,36,93,53,76,5,30,17,39,34,28,16,32,26,46,29,60,15,87,72,44,13,94,19,47,54,14,82,51,12,11,58,73,92,64,49,84,9,27,41,91,20,89,55};
+// InsertionSort.selectionSort(arrB);
+// // System.out.println(Arrays.toString(arrB));
+// System.out.println();
+// InsertionSort.insertionSort(arrB);
+// // System.out.println(Arrays.toString(arrB));
+// int[] arrC = {49,81,31,48,1,34,51,96,40,2,59,32,22,90,26,70,28,17,19,68,21,74,93,5,11,18,4,66,72,58,78,53,12,15,8,77,37,61,24,33,25,41,56,30,62,50,36,89,88,60
+// };
+// InsertionSort.selectionSort(arrC);
+// // System.out.println(Arrays.toString(arrC));
+// System.out.println();
+// InsertionSort.insertionSort(arrC);
+// System.out.println(Arrays.toString(arrC));
+
+    }
+}
